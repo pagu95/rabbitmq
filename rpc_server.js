bc10746bb91adda3260548ee1dc2a67353b29908
root <xristospas1995@hotmail.com>
2019-05-08 03:13:44 +0300

    first commit

---
+++ rpc_server.js
@@ -0,0 +1,66 @@
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function(error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+
+	var myarray = [];
+	
+myarray.push({
+    "id": 1,
+    "color": "blue",
+    "model_name": "ford",
+    "year": 2016
+},{
+    "id": 2,
+    "color": "red",
+    "model_name": "Fiat",
+    "year": 2016
+}, {
+    "id": 3,
+    "color": "blue",
+    "model_name": "Maruti",
+    "year": 2016
+}, {
+    "id": 4,
+    "color": "red",
+    "model_name": "Fiat",
+    "year": 2016
+}, {
+    "id": 5,
+    "color": "red",
+    "model_name": "tata",
+    "year": 2016
+});
+	var queue = 'rpc_queue';
+
+    channel.assertQueue(queue, {
+      durable: false
+    });
+    channel.prefetch(1);
+    console.log(' [x] Awaiting RPC requests');
+    channel.consume(queue, function reply(msg) {
+      var n = parseInt(msg.content.toString());
+
+      console.log(" [.]i got -> (%d)", n);
+	
+	    if(n == 1){
+
+		var r = myarray;
+	 	r = JSON.stringify(myarray);
+	    }
+
+      channel.sendToQueue(msg.properties.replyTo,
+        Buffer.from(r), {
+          correlationId: msg.properties.correlationId
+        });
+
+      channel.ack(msg);
+    });
+  });
+});
